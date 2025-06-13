f5eab96c3d57d607ed30cd25f30906720f165183
David Akinwumi <akdavid4real@gmail.com>
2025-06-13 12:44:55 +0100

    BUILD

---
+++ out/_next/static/chunks/app/layout-60e1947f25a12567.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3110:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.t.bind(n,6413,23))},6413:()=>{},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[904,441,684,358],()=>s(3110)),_N_E=e.O()}]);
